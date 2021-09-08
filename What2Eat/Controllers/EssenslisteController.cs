6c9e49c059517fbc81938023cc03b0bafa626ad3
JayyTime <62748613+JayyTime@users.noreply.github.com>
2021-09-08 21:11:22 +0200

    Essensliste Model, View und Controller erstellt

---
+++ What2Eat/Controllers/EssenslisteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using What2Eat.Models;
+
+namespace What2Eat.Controllers
+{
+    public class EssenslisteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Essensliste()
+        {
+            List<EssenslisteModel> mahlzeiten = new List<EssenslisteModel>();
+            mahlzeiten.Add(new EssenslisteModel { Id = 1, Name = "Croque", Janic = true, Oma = true, Opa = true });
+            mahlzeiten.Add(new EssenslisteModel { Id = 2, Name = "Burger", Janic = true, Oma = true, Opa = true });
+            mahlzeiten.Add(new EssenslisteModel { Id = 3, Name = "Chinesisch", Janic = true, Oma = true, Opa = true });
+
+            return View(mahlzeiten);
+        }
+    }
+}
---
+++ What2Eat/Models/EssenslisteModel.cs
@@ -0,0 +1,15 @@
+﻿namespace What2Eat.Models
+{
+    public class EssenslisteModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Janic { get; set; }
+
+        public bool Oma { get; set; }
+
+        public bool Opa { get; set; }
+    }
+}
